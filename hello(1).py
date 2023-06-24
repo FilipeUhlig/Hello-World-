9545cc8ee6d3b08ac515b8a4ad52d7778df9366b
FilipeUhlig <136847439+FilipeUhlig@users.noreply.github.com>
2023-06-24 10:06:30 -0300

    Add files via upload

---
+++ hello(1).py
@@ -0,0 +1,17 @@
+"""
+Programa: Hello,world!
+Descrição: Este programa imprime na tela a frase "Hello, world!"
+Autor: Filipe
+Data: 02/06/2023
+Versão: 0.0.1
+"""
+#Atribuição de variáveis
+
+#Entrada de dados
+
+
+#Processamento de dados
+
+
+#Saida de dados
+print("Hello,world!")
